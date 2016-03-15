f163121632b4996307e4c16179ae9a0f23e26f53
HeRc010 <adlawren010@gmail.com>
2016-03-14 22:47:28 -0600

    Added test files, xreverse is still a work in progress.

---
+++ family.pl
@@ -0,0 +1,8 @@
+grandparent(X,Z) :- parent(X,Y),parent(Y,Z).
+
+parent(X,Y) :- father(X,Y).
+parent(X,Y) :- mother(X,Y).
+
+father(ken,mary).
+mother(anne,mary).
+mother(mary,john).
--- test.pl
+++ test.pl
@@ -1,9 +1,17 @@
-grandparent(X,Z) :- parent(X,Y), parent(Y,Z).
-parent(X,Y) :- father(X,Y).
-parent(X,Y) :- mother(X,Y).
+xappend([], X, X).
+xappend([A|L1], L2, [A|L3]) :- xappend(L1,L2,L3).
 
-father(ken, mary).
-mother(lily, mary).
-mother(mary, john).
+xadd(X,Y,Z) :- Z is X + Y.
+xsub(X,Y,Z) :- Z is X - Y.
 
-    
+xisempty([]).
+
+xlast([],_).
+xlast([X],X).
+xlast([_|X],Y) :- xlast(X,Y).
+
+xfirst([A|_],Y) :- Y = A.
+
+xreverse([],[]).
+xreverse([X],[X]).
+xreverse([_|L],[A|X]) :- xreverse(L,X),xlast(L,A).
